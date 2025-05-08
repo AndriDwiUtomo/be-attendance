dfe0e63db811206ea138f7c0208bac474baafefd
Andri Dwi utomo <andri@suitmedia.com>
2025-05-08 14:01:28 +0800

    fix: move DB secrets to .env3

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 
-console.log("Port DB:", process.env.DB_PORT);
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  db: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 module.exports = {
   HOST: process.env.DB_HOST,
